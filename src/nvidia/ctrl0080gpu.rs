81a293def0b8527b78326d91bb6cfb3aec9c8483
Coia Prant <coiaprant@gmail.com>
2025-06-03 00:15:35 +0800

    nvidia(ctrl0080gpu): sync vGPU 18.x (R570) fields
    Synchronizing fields for vGPU 18.x (R570)
    
    Signed-off-by: Coia Prant <coiaprant@gmail.com>

--- src/nvidia/ctrl0080gpu.rs
+++ src/nvidia/ctrl0080gpu.rs
@@ -9,4 +9,8 @@ pub const NV0080_CTRL_GPU_VIRTUALIZATION_MODE_HOST: u32 = 0x00000003;
 #[repr(C)]
 pub struct Nv0080CtrlGpuGetVirtualizationModeParams {
     pub virtualization_mode: u32,
+    // R570 adds additional fields, leave them out for now for backwards compat with 16.x and 17.x
+    // https://github.com/NVIDIA/open-gpu-kernel-modules/blob/570/src/common/sdk/nvidia/inc/ctrl/ctrl0080/ctrl0080gpu.h#L313
+    //
+    // pub isGridBuild: bool,
 }
